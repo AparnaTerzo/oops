4cf6f34b0400d598213360824f53217586e3dee0
AparnaTerzo <aparna@terzocloud.com>
2023-06-15 11:09:57 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,42 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
 
-        System.out.printf("Object Oriented programming");
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String expression = sc.nextLine();
+        String[] opr = expression.split("[+-/*]");
+        String[] num= expression.split("[0-9]");
+
+        int a=0;
+        int i=0;
+        for(String str:num) {
+            int b = Integer.parseInt(opr[i]);
+            if (str.equals("+")) {
+                a += b;
+            } else if (str.equals("-")) {
+                a -= b;
+            } else if (str.equals("/") && b != 0) {
+                if(i==0){
+                    a=1;
+                }
+                a /= b;
+            } else {
+                if(i==0){
+                    a=1;
+                }
+                a *= b;
+            }
+            i++;
+
+        }
+        if(num.length==0){
+            a+=Integer.parseInt(opr[0]);
+        }
+        System.out.println(a);
 
     }
+
 }
